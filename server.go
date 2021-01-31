3e2493602562f4b993316e81bee18638d77e0ea0
PlatinBae <gececi1204@gmail.com>
2021-01-31 10:51:35 +0300

    Fix lines and update package 📦

--- server.go
+++ server.go
@@ -1,28 +1,28 @@
 package main
 
 import ( 
-	"github.com/gofiber/fiber/v2"
-	"github.com/joho/godotenv"
-	"log"
-	"os"
+  "github.com/gofiber/fiber/v2"
+  "github.com/joho/godotenv"
+  "log"
+  "os"
 )
 
 func main() {
-	// Load .env file 
-	err := godotenv.Load()
-	if err != nil {
-  	log.Fatal("Error loading .env file")
-	}
+  // Load .env file 
+  err := godotenv.Load()
+  if err != nil {
+    log.Fatal("Error loading .env file")
+  }
 	
-	// Create server
+  // Create server
   app := fiber.New()
 	
-	// Will send html file to main page
+  // Will serve html file to main page
   app.Get("/", func(c *fiber.Ctx) error {
     return c.SendFile("./public/index.html")
   })
 	
-	// Return json message from api
+  // Return json message from api
   app.Get("/api", func(c *fiber.Ctx) error {
     return c.JSON(fiber.Map{
     	"Hello": "World",
@@ -30,13 +30,13 @@ func main() {
   })
   
   // Get PORT from .env file
-	port := os.Getenv("PORT")
+  port := os.Getenv("PORT")
 	
-	// If there's no port will open in 3000
-	if os.Getenv("PORT") == "" {
-		port = "3000"
+  // If there's no port will open in 3000
+  if os.Getenv("PORT") == "" {
+    port = "3000"
 	}
 
-	// And the final touch for the server to work
+  // And the final touch for the server to work
   app.Listen(":" + port)
 }
