a38fd7037f6c60573cce2087af808ebb469516ab
PlatinBae <gececi1204@gmail.com>
2021-01-31 11:20:20 +0300

    Add logging ✨

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.15
 require (
 	github.com/gofiber/fiber/v2 v2.3.3
 	github.com/joho/godotenv v1.3.0
+	github.com/ttacon/chalk v0.0.0-20160626202418-22c06c80ed31
 )
--- server.go
+++ server.go
@@ -1,8 +1,10 @@
 package main
 
-import ( 
+import (
+  "github.com/gofiber/fiber/v2/middleware/logger"
   "github.com/gofiber/fiber/v2"
   "github.com/joho/godotenv"
+  "github.com/ttacon/chalk"
   "log"
   "os"
 )
@@ -11,11 +13,18 @@ func main() {
   // Load .env file 
   err := godotenv.Load()
   if err != nil {
-    log.Fatal("Error loading .env file")
+    log.Fatal(chalk.Red.Color("Error loading .env file"))
   }
 	
   // Create server
   app := fiber.New()
+	
+	// Create Logging
+  app.Use(logger.New(logger.Config{
+    Format:     chalk.Blue.Color("[${time}] ${status} - ${latency} ${method} ${path}\n"),
+    TimeFormat: "02-Jan-2006",
+    TimeZone:   "America/New_York",
+  }))
 	
   // Will serve html file to main page
   app.Get("/", func(c *fiber.Ctx) error {
@@ -26,7 +35,7 @@ func main() {
   app.Get("/api", func(c *fiber.Ctx) error {
     return c.JSON(fiber.Map{
     	"Hello": "World",
-  	})
+    })
   })
   
   // Get PORT from .env file
@@ -35,7 +44,7 @@ func main() {
   // If there's no port will open in 3000
   if os.Getenv("PORT") == "" {
     port = "3000"
-	}
+  }
 
   // And the final touch for the server to work
   app.Listen(":" + port)
